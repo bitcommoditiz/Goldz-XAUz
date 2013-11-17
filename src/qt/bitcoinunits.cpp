22edd47d8eef50867caa2453b48e9ed7a9a35e0e
BitCommoditiz <contact@bitcommoditiz.org>
2013-11-17 18:42:50 +0100

    unit

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("XAUz (Ozz)");
-    case mBTC: return QString("mXAUz (mOzz)");
-    case uBTC: return QString::fromUtf8("μXAUz (μOzz)");
+    case BTC: return QString("XAUz (Troy Ouncez: Ozz)");
+    case mBTC: return QString("mXAUz (mTroy Ouncez: mOzz)");
+    case uBTC: return QString::fromUtf8("μXAUz (μTroy Ouncez μOzz)");
     default: return QString("???");
     }
 }
