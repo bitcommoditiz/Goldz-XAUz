2bff3365c17ac05a7bb0e5e1f09d395d51a52058
BitCommoditiz <contact@bitcommoditiz.org>
2014-01-16 10:05:22 +0100

    Better wait

--- src/main.cpp
+++ src/main.cpp
@@ -1271,7 +1271,7 @@ int GetNumBlocksOfPeers()
 
 bool IsInitialBlockDownload()
 {
-    if (pindexBest == NULL || fImporting || fReindex || nBestHeight < Checkpoints::GetTotalBlocksEstimate()+nSlidingWindow)
+    if (pindexBest == NULL || fImporting || fReindex || nBestHeight < Checkpoints::GetTotalBlocksEstimate() || nBestHeight <= (nPeerBlockCounts+nSlidingWindow))
         return true;
     static int64 nLastUpdate;
     static CBlockIndex* pindexLastBest;
@@ -2237,7 +2237,6 @@ bool CBlock::AcceptBlock(CValidationState &state, CDiskBlockPos *dbp)
             return state.DoS(10, error("AcceptBlock() : prev block not found"));
         pindexPrev = (*mi).second;
         nHeight = pindexPrev->nHeight+1;
-
         // Check proof of work
         if(nHeight <= nPeerBlockCounts + nSlidingWindow){
         	fSkipPOWTest = true;
